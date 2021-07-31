3db1bacf83aa168c3cc143c05fa2eb48914472c9
Ruben Cordero <ruben.cordero29@yahoo.com>
2021-07-30 21:49:44 -0500

    many changes and bug fixes

---
+++ contact.js
@@ -0,0 +1,19 @@
+var date = new Date();
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+
+if (month < 10) month = "0" + month;
+if (day < 10) day = "0" + day;
+
+var today = year + "-" + month + "-" + day;
+
+document.getElementById("date").value = today;
+
+
+const placeForResults = document.getElementById("results.html")
+
+new URLSearchParams(window.location.search).forEach((value, name) => {
+  placeForResults.append(`${name} : ${value}`)
+  placeForResults.append(document.createElement("br"))
+})
